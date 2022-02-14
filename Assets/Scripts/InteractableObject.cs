352581713fd128e631fdedbffdd7d36fac256766
Janis M <janis.meister87@gmail.com>
2022-02-14 15:23:22 +0100

    schule

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ Base Class for all objects with interaction
+ */
+
+public class InteractableObject : MonoBehaviour
+{
+
+}
